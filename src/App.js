47a2dd9e42fed6b396f500b696c1111c09767cb0
ashrithyvs <ashrithy20@gmail.com>
2021-07-13 15:39:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+import { Button, Form, Navbar, Nav } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+// const analytics = firebase.analytics();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar variant="dark" className="p-3">
+          <Navbar.Brand className="mx-2">Sudo ChatRoom</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <SignOut />
+            </Nav.Link>
+          </Nav>
+        </Navbar>
       </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <div className="flex-column justify-content-center">
+      <Button
+        variant="outline-light"
+        className="w-50 my-3"
+        size="sm"
+        onClick={signInWithGoogle}
+      >
+        Sign in with Google
+      </Button>
+      <p>
+        Do not violate the community guidelines or you will be banned for life!
+      </p>
+    </div>
+  );
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <Button variant="outline-light" onClick={() => auth.signOut()}>
+        Sign Out
+      </Button>
+    )
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+      <Form onSubmit={sendMessage}>
+        <Form.Control
+          value={formValue}
+          className="m-1 "
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="Say something!"
+        />
+        <Button
+          variant="outline-light"
+          className="m-1"
+          type="submit"
+          disabled={!formValue}
+        >
+          Send
+        </Button>
+      </Form>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img
+          src={
+            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+          }
+          alt="User profile"
+        />
+        <p className="msg-text">{text}</p>
+      </div>
+    </>
   );
 }
 
